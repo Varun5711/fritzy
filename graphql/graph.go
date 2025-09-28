444440e57907dfe6c9b7dd43ecbb52d762358880
Varun5711 <varunhotani@gmail.com>
2025-09-28 15:22:03 +0530

    feat(grapql): Completed graphql microservice

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,9 +1,63 @@
 package main
 
-// type Server struct {
-// 	accountClient
-// 	catalogClient
-// 	orderClient
+import "github.com/99designs/gqlgen/graphql"
+
+type Server struct {
+	// accountClient *account.Client
+	// catalogClient *catalog.Client
+	// orderClient   *order.Client
+}
+
+func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+	// 	accountClient, err := account.NewClient(accountUrl)
+
+	// 	if err != nil {
+	// 		accountClient.Close()
+	// 		return nil, err
+	// 	}
+
+	// 	catalogClient, err := catalog.NewClient(catalogUrl)
+
+	// 	if err != nil {
+	// 		catalogClient.Close()
+	// 		return nil, err
+	// 	}
+
+	// 	orderClient, err := catalog.NewClient(orderUrl)
+
+	// 	if err != nil {
+	// 		accountClient.Close()
+	// 		catalogClient.Close()
+	// 		return nil, err
+	// 	}
+
+	return &Server{
+		// accountClient,
+		// catalogClient,
+		// orderClient,
+	}, nil
+}
+
+// func (s *Server) Mutation() MutationResolver {
+// 	return &mutationResolver{
+// 		server: s,
+// 	}
 // }
 
-func NewGraphQLServer() {}
+// func (s *Server) Query() QueryResolver {
+// 	return &queryResolver{
+// 		server: s,
+// 	}
+// }
+
+// func (s *Server) Account() AccountResolver {
+// 	return &accountResolver{
+// 		server: s,
+// 	}
+// }
+
+func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		// Resolvers: s,
+	})
+}
--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,1 +1,19 @@
 package main
+
+// import "context"
+
+type mutationResolver struct {
+	server *Server
+}
+
+// func (r *mutationResolver) CreateAccount(ctx context.Context, in Account) (*Account, error) {
+
+// }
+
+// func (r *mutationResolver) CreateProduct(ctx context.Context, in Product) (*Product, error) {
+
+// }
+
+// func (r *mutationResolver) CreateOrder(ctx context.Context, in Order) (*Order, error) {
+
+// }
